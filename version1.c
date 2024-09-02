be210ab058b28822120f2b1a8d4228f28c8af744
github9364 <ghub4551@gmail.com>
2024-09-02 09:09:58 -0600

    version1.c

--- version1.c
+++ version1.c
@@ -2,25 +2,33 @@
 
 int main()
 {
-	int a,b;
-	int sum,rest,mult,div;
+	int a, b, c, c1, c2, c3;
 	
-	printf("ingrese el primer valor (entero): \n");
+	printf("Este programa suma, resta, multiplica y divide 2 numeros \n");
+	
+	printf("Ingresa el primer numero : \n");
 	scanf("%d",&a);
 	
-	printf("ingrese el segundo valor (entero): \n");
+	printf("Ingresa el segundo numero : \n");
 	scanf("%d",&b);
 	
-	sum= a+b;
-	rest= a-b;
-	mult= a*b;
-	div= a/b;
+	c=a+b;
+	c1=a-b;
+	c2=a*b;
 	
-	printf("el resultado de la suma es: %d \n",sum);
-	printf("el resultado de la resta es: %d \n",rest);
-	printf("el resultado de la multiplicacion es: %d \n",mult);
-	printf("el resultado de la division es: %d \n",div);
+	if(c3 < 0)
+	{
+		printf(" \n Error en la division \n ");
+	}
+	else
+	{
+		c3=a/b;
+		printf("\n El resultado de la division es: %d ",c3);
+	}
 	
+	printf("\n El resultado de la suma es: %d ",c);
+	printf("\n El resultado de la resta es: %d ",c1);
+	printf("\n El resultado de la multiplicacion es: %d ",c2);
 	
 	return 0;
 }
